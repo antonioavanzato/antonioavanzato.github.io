db001233d1fe42b078493f5b4c96b6ec70ae6545
Avanzato <81472009+antonioavanzato@users.noreply.github.com>
2026-06-07 15:15:56 +0300

    Update site.js

--- scripts/site.js
+++ scripts/site.js
@@ -21,21 +21,21 @@
   var current = document.body.getAttribute('data-page') || 'home';
 
   var PORTFOLIO = [
-    { href:'https://velaatrips.com',     title:'Velaa Trips',      desc:'туристическое агентство', size:'tall',
+    { href:'https://velaatrips.com',     title:'Velaa Trips',      desc:'туристическое агентство на Мальдивах', size:'tall',
       src:'https://i.wfolio.ru/x/lXTL47Bxbdssl6M0l438gwfsRu0n-L2O/4IHWP6hz8oYlwwMqeiEvXDQW4opBV45E/tEz0XtzRpiAZcSbGA1Z9N51wfS3ixLBN/oKP1bEK7mRl6ZeOYbTYF3-509KJWHpgc/5XA-R3ptFbJGYY8WlHQvphi8Hxl9KEmu/EYDWE_OGVuDR3v60kr0YtYs7INstH6We.png' },
     { href:'https://darialukianova.com', title:'Дарья Лукьянова',  desc:'преподаватель из Казани',  size:'',
       src:'https://i.wfolio.ru/x/5r-vL37XfWIZzOz1-h8uW-kY1GWQLfMW/j8zKohu0GYi6Of2-q2Y2sr1NUx1W78Q0/9A2t_t85Qb9M5U4GSY21PB0hJnc4eXiF/mkksxtxuGYbgVPG5wPQX5hRy5mv-8Z_j/B8nzOdTDBVvGlgqjQGvGE1ACf1p1ICoR/fNj0veEE7HAu35MB9Vl3y1mQMpjRq0Hj.png' },
     { href:'https://crisavis.ru',        title:'Crisavis',         desc:'фотограф из Испании',     size:'',
       src:'https://i.wfolio.ru/x/D48ScH1DA6jxU_uSH-Or-C0cJClpP-QP/lEAL128xEMPGM2o_rKw_kMPHKR9vAJ80/BNWL1PFjI2B_pF_Xqq-Ohmjc29dKFonr/bVBkTR9F-0L8cVUyqYPu2uCIghfgHzPx/cRdXVYfMyf189M4aNGWwng.png' },
-    { href:'https://portanobile.com',    title:'Porta Nobile',     desc:'люксовая фурнитура',      size:'tall',
+    { href:'https://portanobile.com',    title:'Porta Nobile',     desc:'люксовая фурнитура в Мокве',      size:'tall',
       src:'https://i.wfolio.ru/x/D7KuMwGO6ZJnvKA0F1dwJp5uSVA-rZqe/SpT1sdsTYByNdOFW2ulOEwPIdZBIDTeF/CpiO-zgmZEMSBhmBQAL423MfuZ8spudM/ndaILxMwcTarj5nHHvsmAng2U-wJoeU5/j36BYXlVLjwyz0c_hM6v3n1ow1LwwG1_/UX6n509YPcIL8ogdQc0u0PRMqxcuX2nu.png' },
-    { href:'https://medfitclub.ru',      title:'Medical Fitness',  desc:'центр реабилитации',      size:'',
+    { href:'https://medfitclub.ru',      title:'Medical Fitness',  desc:'центр реабилитации в Москве',      size:'',
       src:'https://i.wfolio.ru/x/D48ScH1DA6jxU_uSH-Or-C0cJClpP-QP/lEAL128xEMPGM2o_rKw_kMPHKR9vAJ80/bRL2GL1rwUSi8wAkvDr45cVup18TXz3t/3M8CG6q0Jghep-4HDUXmpGBtJhrCPA4a/SWqOO2l6zohPLH4NDKunEg.png' },
-    { href:'https://alga-tour.com',      title:'ALGA',             desc:'туристическое агентство', size:'',
+    { href:'https://alga-tour.com',      title:'ALGA',             desc:'туристическое агентство на Мальдивах', size:'',
       src:'https://i.wfolio.ru/x/nSMVNbIkpj73jijrmuEgqe-OtrGkDdVw/ksqqOw-68Y_04lrUcq-dUDIvosZl09Np/4l0rOaVfphEg2Z4oi2v7oR9KfKVYnmX7/e97SN9ounT4GQKu4xzMoO_x26ZYQQRxx/I34FSDXkpPaNo0ZuZBQKIQ7Nk2NRnBSy/htkIY0fLUH4ckbvGcf9tPw5eiJTXz5T_.png' },
-    { href:'https://maldives-elite.ru',  title:'Maldives Elite',   desc:'туристическое агентство', size:'tall',
+    { href:'https://maldives-elite.ru',  title:'Maldives Elite',   desc:'туристическое агентство на Мальдивах', size:'tall',
       src:'https://i.wfolio.ru/x/D48ScH1DA6jxU_uSH-Or-C0cJClpP-QP/lEAL128xEMPGM2o_rKw_kMPHKR9vAJ80/P4QgaGJvLpYV86gxnT7KQ14RlcGNBAU-/QJwc8gsax6C_VVgQiVb0G7QdwKtddgpn/i5VTe7Q4cuSMY3azx5sRog.png' },
-    { href:'https://yanapro.ru',         title:'Яна Самойлова',    desc:'фитнес-тренер',           size:'',
+    { href:'https://yanapro.ru',         title:'Яна Самойлова',    desc:'фитнес-тренер в Казани',           size:'',
       src:'https://i.wfolio.ru/x/IpIDXWsy6fAubMzqJy3peT9C_63INnIW/uPNqZb6-pqtDzBlZ-MRJEQlHRPvoh3IF/7skTmO4p5_xtjZFaAvJwPXbpKTGmEDGn/2PnckuH3i2QBSlL4uB5dBwzgPiY8FSx8/2uFEfFkivs3fpFfgEQgM0GKcKZ_ccFRQ/4UA5Qlv2Wuo91JcekPwnXZoD10he_811.jpeg' }
   ];
   window.AVW_PORTFOLIO = PORTFOLIO;
